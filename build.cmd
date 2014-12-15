8eb12bd90ea25dbea02fa00d451d77f0973d29e3
babumuralidharan <babumuralidharan@users.noreply.github.com>
2014-12-14 23:14:27 -0800

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -32,6 +32,38 @@ XCOPY /Y/I/Q/S Inst4WA.Test\TestCases\* build\Debug\TestCases
 XCOPY /Y/I/Q/S build\Debug\Resources\* build\Debug
 RMDIR /S/Q build\Debug\Resources
 
+setlocal
+
+if "%1"=="/?" goto usage
+if "%1"=="-?" goto usage
+
+@echo Building Inst4WA and for external release
+@echo Deleting previous build
+
+if exist build rmdir /S/Q build
+
+@echo Building Debug
+set Configuration=Debug
+
+msbuild Inst4WA.sln
+if errorlevel 1 goto error
+
+mkdir build\Debug\TestData
+rem mkdir build\Debug\TestData\SolrPkg\SolrAdminWebRole
+rem mkdir build\Debug\TestData\SolrPkg\SolrMasterHostWorkerRole
+rem mkdir build\Debug\TestData\SolrPkg\SolrSlaveHostWorkerRole
+rem mkdir build\Debug\TestData\CouchDbPkg\WorkerRole
+
+XCOPY /Y/I/Q/S Inst4WA.Test\TestData\WebRole build\Debug\TestData\WebRole
+XCOPY /Y/I/Q/S Inst4WA.Test\TestData\WorkerRole build\Debug\TestData\WorkerRole
+XCOPY /Y/I/Q/S Inst4WA.Test\TestData\VMRole build\Debug\TestData\VMRole
+XCOPY /Y/I/Q/S Inst4WA.Test\bin\Debug build\Debug
+XCOPY /Y/I/Q/S Inst4WA.Test\TestCases\* build\Debug\TestCases
+
+@echo Copy contents of Resources directory to root
+XCOPY /Y/I/Q/S build\Debug\Resources\* build\Debug
+RMDIR /S/Q build\Debug\Resources
+
 COPY /Y Inst4WA.Test\Inst4WaExe\Inst4WA.exe build\Debug\Inst4WA.exe
 COPY /Y Build.testsettings build\Debug\Build.testsettings
 COPY /Y Inst4WA.Test\Azure.publishsettings  build\Debug
